f50e9b8d2a32d7b51a5a91878bd2578fc2b5a9cd
Abdul Waqar <63345475+abdulwaqar844@users.noreply.github.com>
2021-01-24 00:10:05 +0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,16 +28,6 @@ module.exports = {
       },
     },
      "gatsby-plugin-typescript",
-     {
-      resolve: "gatsby-source-graphql",
-      options: {
-        // Arbitrary name for the remote schema Query type
-        typeName: "GETLOLLY",
-        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
-        fieldName: "getlolly",
-        // Url to query from
-        url: "https://virtual-lolly-app.netlify.app/.netlify/functions/GraphQL",
-      },
-    },
+    
   ],
 }
