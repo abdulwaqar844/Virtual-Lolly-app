8db3d51163eda813650bf50ccfd1e6a6b43e25f8
Abdul Waqar <abdulwaqar844@gmaill.com>
2021-01-24 00:10:49 +0500

    style update

--- gatsby-node.js
+++ gatsby-node.js
@@ -23,12 +23,11 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     return
   }
   result.data.getlolly.getLolly.forEach((d) => {
-    console.log("++++++++++++++++ page created", d)
     createPage({
       path: `/lollies/${d.link}`,
       component: path.resolve(`./src/components/template.js`),
       context: {
-        data: d
+         d
       },
     });
   });
--- src/components/template.js
+++ src/components/template.js
@@ -1,32 +1,36 @@
 import React from 'react'
 import Lolly from "./lolly"
+import "./template.css"
 import { navigate } from 'gatsby';
-export default function Template(pageContext) {
-  let data =pageContext.pathContext  ;
-//console.log(data)
- /* if (loading)
-    return <h2>loading..</h2>
-  if (error)
-    return <h2>error</h2>*/
+export default function Template({ pageContext }) {
+  console.log(pageContext.d.link)
+  //console.log(data)
+  /* if (loading)
+     return <h2>loading..</h2>
+   if (error)
+     return <h2>error</h2>*/
   return (
     <>
-      <div>
-        <h1>Share lolly with this link:</h1>
-        <p>{`http://localhost:8888/lollies/${data.link.link}`}</p>
-            <div className="container-lolly">
-{           <div key={data.link.id} className="display-lolly">
-                <div className="lol">
-                  <Lolly top={data.link.c1} middle={data.link.c2} bottom={data.link.c3} />
-                </div>
-                <div className="resultCard">
-                  <p className="reciever">To:{data.link.rec}</p>
-                  <p className="message">Message:{data.link.message}</p>
-                  <p className="sender">From:{data.link.sender}</p>
-                </div>
-  </div>}
+      <div className="container">
+        <div className="container-lolly">
+          <div key={pageContext.d.id} className="display-lolly">
+            <div className="lol">
+              <Lolly top={pageContext.d.c1} middle={pageContext.d.c2} bottom={pageContext.d.c3} />
+            </div>
+              <div>
+              <p>Share lolly with this link:</p>
+        <p className="share">{`http://localhost:8888/lollies/${pageContext.d.link}`}</p>
+        <div className="details">
+
+              <p className="reciever">To:{pageContext.d.rec}</p>
+              <p className="message">Message:{pageContext.d.message}</p>
+              <p className="from">From:{pageContext.d.sender}</p>
+            </div>
+              </div>
+          </div>
 
 
-            </div>   
+        </div>
 
 
         <button onClick={() => navigate("/")}>Go Back</button>
